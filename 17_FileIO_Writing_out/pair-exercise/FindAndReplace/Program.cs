122a6835c887db40bf5c556fe01529521e2759e1
Carter Smith <cartersmith19013@gmail.com>
2019-10-08 14:22:15 -0400

    exercise17_Complete

--- 17_FileIO_Writing_out/pair-exercise/FindAndReplace/Program.cs
+++ 17_FileIO_Writing_out/pair-exercise/FindAndReplace/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace FindAndReplace
 {
@@ -6,7 +7,50 @@ namespace FindAndReplace
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //Inputs
+            Console.Write("Hello! What is the fully qualified name of the file would you like to search today? " );
+            string sourceFile = Console.ReadLine();
+            Console.WriteLine();
+            if (!File.Exists(sourceFile))
+            {
+                Console.WriteLine("This is not a valid name. Please type in the fully qualified name of the desired file. " );
+                sourceFile = Console.ReadLine();
+            }
+            if (File.Exists(sourceFile))
+            {
+                Console.Write("What phrase would you like to search? " );
+                string searchPhrase = Console.ReadLine();
+                Console.WriteLine();
+
+                Console.Write("What would you like to replace this phrase with? " );
+                string replacePhrase = Console.ReadLine();
+                Console.WriteLine();
+
+                Console.Write("What is the fully qualified name of the file would you like to copy the changed text into? " );
+                string destinationFile = Console.ReadLine();
+                Console.WriteLine();
+
+                //Copying Over Source File --> Destination File w/Changes
+                using (StreamReader sr = new StreamReader(sourceFile))
+                {
+                    using (StreamWriter sw = new StreamWriter(destinationFile, true))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            string fixedLine = line.Replace(searchPhrase, replacePhrase);
+                            sw.Write(fixedLine);
+                        }
+                    }
+                }
+            }
+            
+            
+            
         }
+
+
     }
 }
+
