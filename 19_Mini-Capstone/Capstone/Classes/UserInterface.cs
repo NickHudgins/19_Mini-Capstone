9630065c17bb17ec734cf5b46be356c343f461c6
Nick Hudgins <nickhudgins@gmail.com>
2019-10-10 13:46:57 -0400

    capstone progress commit -NH

--- 19_Mini-Capstone/Capstone/Classes/UserInterface.cs
+++ 19_Mini-Capstone/Capstone/Classes/UserInterface.cs
@@ -14,13 +14,79 @@ namespace Capstone.Classes
 
         public void RunInterface()
         {
-            bool done = false;
-            while (!done)
+            Console.WriteLine("To display available Catering Items: press 1 ");
+            Console.WriteLine("To Order: press 2 ");
+            Console.WriteLine("To quit: press 3 ");
+
+            string selection = Console.ReadLine();
+
+            while (selection != "3")
             {
-                Console.WriteLine("This is the UserInterface");
-                Console.ReadLine();
+                switch (selection)
+                {
+                    case "1":
+                        CateringItem();
+                        break;
+
+                    case "2":
+                        Catering();
+                        break;
+
+                    default:
+                        Console.WriteLine("Please Enter Valid Selection. ");
+                        break;
+                }
+
             }
+
+        }
+
+
+        public void OrderingInterface()
+        {
+            Console.WriteLine("To add money: press 1 ");
+            Console.WriteLine("To select products: press 2 ");
+            Console.WriteLine("To complete transaction, press 3 ");
+            Console.WriteLine("Your current balance is: ");
+
+            string selection = Console.ReadLine();
+
+            while (selection != "3")
+            {
+                switch (selection)
+                {
+                    case "1":
+                        AddMoney();
+                        break;
+
+                    case "2":
+                        ProductSelection();
+                        break;
+
+                    default:
+                        Console.WriteLine("Please Enter Valid Selection. ");
+                        break;
+                }
+            }
+        }
+        public void AddMoney()
+        {
+            Console.WriteLine("Please enter amount to desposit. ");
+            string amountDeposited = Console.ReadLine();
+
+            while
+            {
+
+            }
+
+        }
+        public void ProductSelection()
+        {
+            Console.WriteLine("Please enter the product code you'd like to add to your cart. ");
+            string codeEntered = Console.ReadLine();
+
 
         }
     }
 }
+
